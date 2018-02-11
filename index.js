c9679ac8ef76ec7470e7395b93669e04e200437e
Hieupv <hieupv@codersvn.com>
2018-02-12 00:47:10 +0700

    add select currency step

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 import * as _ from "lodash";
 import moment from "moment";
 import Login from "./app/Services/Login";
+import { dirname } from "path";
 
 const { Builder, By, Key, until } = require("selenium-webdriver");
 require("dotenv").config();
 
-(async function dice() {
+let base_amount = Number(process.env.BASE_AMOUNT);
+let old_value;
+let lose = 0;
+
+const dice = async () => {
   let driver = await new Builder().forBrowser("chrome").build();
   await Login(process.env.EMAIL, process.env.PASSWORD, driver);
-  let base_amount = Number(process.env.BASE_AMOUNT);
   try {
     let amount = _.clone(base_amount);
     await driver.get(process.env.GAME_URL);
-    await driver.sleep(2000);
-    let old_value;
-    let lose = 0;
+    await driver.sleep(5000);
+
+    let curren_currency_classes = await driver.findElement(By.css(process.env.CURRENT_CURRENCY_SELECTOR)).getAttribute("class");
+    if (curren_currency_classes.indexOf(process.env.CURRENCY) === -1) {
+      console.log("Open currency selector");
+      await driver.findElement(By.css(process.env.CHANGE_CURRENCY_SELECTOR)).click();
+      await driver.sleep(2000);
+      await driver.findElement(By.css(`${process.env.CURRENCY_ITEM_SELECTOR}[data-currency='${process.env.CURRENCY}']`)).click();
+      console.log("Select currency");
+      await driver.sleep(2000);
+    }
+
     while (true) {
       console.log(`Start ${moment().format("h:mm:ss")} | Bet Amount: ${amount}`);
       while (true) {
@@ -40,14 +53,21 @@ require("dotenv").config();
       if (is_win || lose === Number(process.env.MAX_LOSE_TIME)) {
         lose = 0;
         amount = _.clone(base_amount);
-        console.log(`----Win ${wallet_ammount}----`);
+        console.log(`----Win, Amount: ${wallet_ammount}----`);
       } else {
         lose++;
         amount = 2 * amount;
-        console.log(`----Lose ${wallet_ammount}----`);
+        console.log(`----Lose(${lose} times), Amount: ${wallet_ammount}----`);
       }
+      // let profit_amount = await driver.findElement(By.css(process.env.PROFIT_AMOUNT_SELECTOR)).getText();
+      // if (Number(profit_amount) > Number(process.env.RESET_WHEN_PROFIT_MORE_THAN)) {
+      //   break;
+      // }
+      // dice();
     }
   } finally {
     // await driver.quit();
   }
-})();
+};
+
+dice();
